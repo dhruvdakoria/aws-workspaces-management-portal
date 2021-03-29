1cc3054a0883d23ead1dd20d7f7ccd69fbc0432f
dhruvdakoria <dhruvdako98@gmail.com>
2021-03-28 23:39:10 -0400

    update approval email

--- website/js/config.js
+++ website/js/config.js
@@ -8,6 +8,6 @@ window._config = {
         invokeUrl: 'https://hfl3wxmzn2.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
-        email: 'aws.dhruv@gmail.com'
+        email: 'aws.cloudframeworks@gmail.com'
     }
 };
